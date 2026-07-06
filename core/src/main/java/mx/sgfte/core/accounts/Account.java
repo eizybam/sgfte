0ee6b92608b9e5e9395993b048d2d9b078b9b636
eizybam <eizybam@outlook.com>
2026-07-06 08:00:46 -0600

    feat(accounts): Added Account class skeleton

---
+++ core/src/main/java/mx/sgfte/core/accounts/Account.java
@@ -0,0 +1,15 @@
+package mx.sgfte.core.accounts;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private final String id;
+    private final String purpose;
+    private BigDecimal balance;
+
+    public Account(String id, String purpose) {
+        this.id = id;
+        this.purpose = purpose;
+        this.balance = BigDecimal.ZERO;
+    }
+}
