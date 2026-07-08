92676eba563a1bf98b3655f894857f10d4917853
eizybam <eizybam@outlook.com>
2026-07-08 07:20:51 -0600

    feature(accounts): added initial tests

--- core/src/main/java/mx/sgfte/core/accounts/Account.java
+++ core/src/main/java/mx/sgfte/core/accounts/Account.java
@@ -16,6 +16,22 @@ public class Account {
         if  (amount.signum() <= 0) {
             throw new IllegalArgumentException("amount must be positive");
         }
-        balance = balance.add(amount);
+        this.balance = this.balance.add(amount);
     }
+
+    public void withdraw(BigDecimal amount) {
+        if  (amount.signum() <= 0) {
+            throw new IllegalArgumentException("amount must be positive");
+        }
+
+        if (amount.compareTo(this.balance) > 0) {
+            throw new IllegalStateException("Insufficient balance");
+        }
+
+        this.balance = this.balance.subtract(amount);
+    }
+
+    public String getId()          { return id; }
+    public String getPurpose()     { return purpose; }
+    public BigDecimal getBalance() { return balance; }
 }
